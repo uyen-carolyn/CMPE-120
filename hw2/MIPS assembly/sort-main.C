3d45fd6f77f52ff44f677503f7240a436cde2b75
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-10-09 21:18:40 -0700

    Create sort-main.C

---
+++ hw2/MIPS assembly/sort-main.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+char * my_name = "Uyen Nguyen";
+int a[]={33, 4, 78, 100, -12};
+extern void sort(int a[], int size);
+void print_array(int a[], int size) {
+  int i;
+  for (i=0; i< size; i++)
+    printf("%d, ", a[i]);
+  printf("\n");}
+void main() {
+  printf("Bubble sort demo by %s at %s %s\n", my_name, __DATE__, __TIME__);
+  printf("before sorting : ");
+  print_array(a, 5);
+  sort(a, 5);
+  printf("after sorting : ");
+  print_array(a, 5);
+}
